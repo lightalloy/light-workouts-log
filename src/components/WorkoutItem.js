d566b70260fe7b8d064cf5904184537496655683
Anna Buyanova <lightallloy@gmail.com>
2018-04-14 15:37:25 +0300

    Display dates

--- src/components/WorkoutItem.js
+++ src/components/WorkoutItem.js
@@ -12,10 +12,18 @@ export class WorkoutItem extends React.Component {
   state = {}
 
   render() {
+    let date = new Date(this.props.workout.time);
+    let month = date.getMonth() + 1;
+    month = month < 10 ? `0${month}` : month;
+    let day = date.getDate();
+    day = day < 10 ? `0${day}` : day;
     return (
-      <Text style={{fontSize: 30}}>
-        <Emoji name={this.props.workout.workoutType} />
-      </Text>
+      <View>
+        <Text>{`${day}.${month}`}</Text>
+        <Text style={{fontSize: 40}}>
+          <Emoji name={this.props.workout.workoutType} />
+        </Text>
+      </View>
     );
   }
 }
